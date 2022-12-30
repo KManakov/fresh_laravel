15e8af08e9d5e80e6d7801e55925c5fc3b86d233
Kmanakov <Kmanakov>
2022-12-30 12:05:12 +0500

    request class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'category_id'];
 
     public function category()
     {
